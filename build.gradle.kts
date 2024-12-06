b8a42ac2c9ff8494307a31d7e9d23ea6e408ee18
fares97203 <fares.hamza97203@gmail.com>
2024-12-06 10:35:14 +0100

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.2")
+    }
+}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
+}
+
+fun implementation(platform: Dependency) {
+
+}
+
+fun implementation(platform: String) {
+
+}
+
